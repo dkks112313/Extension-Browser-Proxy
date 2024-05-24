cc4c6616fa9fd7e417844b16d98878f8a981cf57
dkks112313 <94787767+dkks112313@users.noreply.github.com>
2024-05-25 00:26:07 +0300

    dssd

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const button = document.getElementById('grb_butt');
+button.style.background = "linear-gradient(#ff3232, #deff00)";
+
+let isClick = true;
+
+chrome.storage.sync.get('isClick', (data) => {
+    isClick = data.isClick;
+    changeColor();
+});
 
 button.addEventListener('click', () => {
-    let background;
-    if(button.value === "1") {
-        background = "linear-gradient(#009c4a, #45b5fd)";
-        button.value = "2";
-    }
-    else {
-        background = "linear-gradient(#ff3232, #deff00)";
-        button.value = "1";
-    }
+    isClick = !isClick;
+    changeColor();
 
-    button.style.background = background;
-
-    chrome.storage.sync.set({buttonState: button.value, buttonBackground: background}, function() {
-        console.log('Button state is ' + button.value);
-        console.log('Button background is ' + background);
-    });
-
+    chrome.storage.sync.set({isClick: isClick});
     chrome.runtime.sendMessage({message: "toggleProxy"});
 });
 
-window.onload = function() {
-    chrome.storage.sync.get(['buttonState', "buttonBackground"], function(data) {
-        button.value = data.buttonState;
-        button.style.background = data.buttonBackground;
-    });
-};
+function changeColor() {
+    let background;
+    if(isClick) {
+        background = "linear-gradient(#009c4a, #45b5fd)";
+    }
+    else {
+        background = "linear-gradient(#ff3232, #deff00)";
+    }
+    button.style.background = background;
+}
